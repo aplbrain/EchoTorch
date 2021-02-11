1a7533f358cf08220445e4817b54dc78163fdb7a
seanmcdanielgray <seanmcdgray@gmail.com>
2021-02-11 18:56:14 +0000

    script to explore loading up hsbm graphml R files

--- utils/networkx_graphml_example.py
+++ utils/networkx_graphml_example.py
@@ -1,16 +1,30 @@
-from networkx import read_graphml
+import networkx as nx
 import os 
+import numpy as np
+import matplotlib.pyplot as plt
 
 path = '/home/mcdansl1/Data/hsbm'
 
 list_of_graphml_files = os.listdir(path) # returns list
 
-aGraph = read_graphml(path + '/' + list_of_graphml_files[0])
+G = nx.read_graphml(path + '/' + list_of_graphml_files[0])
 
-print(aGraph)
+'''
+print(G)
 
-for k in aGraph.edges():
+for k in G.edges():
     print(k)
 
-for k in aGraph.nodes():
+for k in G.nodes():
     print(k)
+'''
+
+# converts A's networkx to adj matrix to numpy numpy array
+A = nx.to_numpy_matrix(G)
+
+np.savetxt('test.out', A, delimiter=',', fmt='%i') # specity int format for write out
+
+layout = nx.spring_layout(G)
+nx.draw(G, layout)
+nx.draw_networkx_labels(G,pos=layout)
+plt.savefig('netx_grapml.png')
